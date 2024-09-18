73832765124bcf1a19239eaa806480cfc8562c36
coffeecionado <179670134+coffeecionado@users.noreply.github.com>
2024-09-18 10:37:58 -0500

    Fixing bugs

--- extension.js
+++ extension.js
@@ -91,28 +91,6 @@ class Penguin extends PanelMenu.Button
 
             this.chatInput.set_reactive(false)
             this.chatInput.set_text("I am Thinking...")
-
-            this.timeoutResponse = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, () => { 
-                if (this.chatInput.get_text("I am Thinking...")) {
-                    let response = "Connection time out. Check your internet connection.";
-
-                    this.initializeTextBox('llmMessage', response);
-                    this.chatInput.set_reactive(true)
-                    this.chatInput.set_text("")
-
-                    if (this.timeoutResponse) {
-                        GLib.Source.remove(this.timeoutResponse);
-                        this.timeoutResponse = null;
-                    }
-                }
-                else {
-                    if (this.timeoutResponse) {
-                        GLib.Source.remove(this.timeoutResponse);
-                        this.timeoutResponse = null;
-                    }
-                }
-                });
-            
         });
 
         this.newConversation = new St.Button({ 
@@ -124,7 +102,7 @@ class Penguin extends PanelMenu.Button
         });
 
         this.newConversation.connect('clicked', (actor) => {
-            if (this.chatInput.get_text() == "Create a new conversation (Deletes current)") {
+            if (this.chatInput.get_text() == "Create a new conversation (Deletes current)" ||  this.chatInput.get_text() != "I am Thinking...") {
                 this.history = []
 
                 this.chatBox.destroy_all_children()
@@ -198,6 +176,7 @@ class Penguin extends PanelMenu.Button
         this.menu.addMenuItem(popUp);
     };
 
+    
     openRouterChat() {
         let message = Soup.Message.new('POST', url);
         
@@ -212,6 +191,32 @@ class Penguin extends PanelMenu.Button
 
 
         message.set_request_body_from_bytes('application/json', bytes);
+
+        this.timeoutResponse = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, () => { 
+            if (this.chatInput.get_text() == "I am Thinking...") {
+                let response = "Connection time out. Check your internet connection.";
+
+                this.initializeTextBox('llmMessage', response);
+                this.chatInput.set_reactive(true)
+                this.chatInput.set_text("")
+
+                if (this.timeoutResponse) {
+                    GLib.Source.remove(this.timeoutResponse);
+                    this.timeoutResponse = null;
+                }
+
+                return;
+            }
+            else {
+                if (this.timeoutResponse) {
+                    GLib.Source.remove(this.timeoutResponse);
+                    this.timeoutResponse = null;
+                }
+
+                return;
+            }
+            });
+
         this._httpSession.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (_httpSession, result) => {
             let bytes = _httpSession.send_and_read_finish(result);
             let decoder = new TextDecoder('utf-8');
@@ -222,16 +227,16 @@ class Penguin extends PanelMenu.Button
             log(url);
 
             if (res.error?.code == 401) {
-                let response = "Hmm... It seems like your API key is not present or is incorrect. You can type it here or in the extension settings. Click below to enter your API key and view the guide on how to get one.";
+                let response = "Hmm... It seems like your API key is not present. You can type it here or in the extension settings. Click below to enter your API key and view the guide on how to get one.";
 
                 let final = convertMD(response);
                 this.initializeTextBox('llmMessage', final);
 
                 let settingsButton = new St.Button({
-                    label: "Click here to set up your API for connecting to the chatbot", can_focus: true,  toggle_mode: true
-                });
-                
+                    label: "Click here to set up your API for connecting to the chatbot", can_focus: true,  toggle_mode: true});
+        
                 settingsButton.connect('clicked', (self) => {
+                    log("Hi, working?");
                     this.openSettings();
                 });
 
@@ -242,23 +247,13 @@ class Penguin extends PanelMenu.Button
                 return;
             }
             if(res.error?.code != 401 && res.error !== undefined){
-                let response = "Oh no! It seems like the LLM model you entered is either down or not correct. Make sure you didn't make any errors when inputting it in the settings. You can always use the default extension model (sent in the next message). Check your connection either way";
+                let response = "Error, try another model or check your connection";
 
                 this.initializeTextBox('llmMessage', response);
-                this.initializeTextBox('llmMessage', "mattshumer/reflection-70b:free");
+                this.chatInput.set_reactive(true);
+                this.chatInput.set_text("");
 
-                let settingsButton = new St.Button({
-                    label: "Click here to check or change your model ID", can_focus: true,  toggle_mode: true
-                });
-        
-                settingsButton.connect('clicked', (self) => {
-                    this.openSettings();
-                });
-
-                this.chatBox.add_child(settingsButton)
-
-                this.chatInput.set_reactive(true)
-                this.chatInput.set_text("")
+                return;
             }
             else {
                 let response = res.choices[0].message.content;
@@ -274,6 +269,8 @@ class Penguin extends PanelMenu.Button
 
                 this.chatInput.set_reactive(true)
                 this.chatInput.set_text("")
+
+                return;
             }
         });
 
@@ -309,7 +306,7 @@ class Penguin extends PanelMenu.Button
             label.connect('enter-event', (actor) => {
 
                 
-                if (this.chatInput.get_text() != "I am Thinking...") {
+                if (this.chatInput.get_text() == "") {
                     this.timeoutCopy = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 0.4, () => { 
                         this.chatInput.set_reactive(false);
                         this.chatInput.set_text("Click on text to copy");});
@@ -322,7 +319,7 @@ class Penguin extends PanelMenu.Button
                     this.timeoutCopy = null;
                 }
 
-                if (this.chatInput.get_text() != "I am Thinking...") {
+                if (this.chatInput.get_text() == "Click on text to copy") {
                     this.chatInput.set_reactive(true);
                     this.chatInput.set_text("");
                 }
